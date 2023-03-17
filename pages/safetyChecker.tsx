8bcfdd745457332833d444d64b2aebae5647acf8
alaidine <aa.alimlinde@gmail.com>
2023-03-17 09:42:46 +0400

    you can navigate back to the home page from the safety checker page

--- pages/safetyChecker.tsx
+++ pages/safetyChecker.tsx
@@ -1,1 +1,12 @@
-export default function safetyChecker() {}
+import { Button, Typography } from "@mui/material";
+
+export default function safetyChecker() {
+  return (
+    <>
+      <Button onClick={() => (window.location.href = "/")} variant="outlined">
+        Home
+      </Button>
+      <Typography>Safety checker page</Typography>
+    </>
+  );
+}
