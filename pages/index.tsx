27d4730c2c6515ec4bf091b64f92dfedd6c26fc8
alaidine <aa.alimlinde@gmail.com>
2023-03-17 09:41:54 +0400

    you can now navigate to tha password generator page and the safety checker page with the push of a button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 export default function Home() {
   return (
@@ -17,10 +17,20 @@ export default function Home() {
             width: "300px",
           }}
         >
-          <Button variant="outlined" sx={{ color: "black" }}>
+          <Button
+            onClick={() => (window.location.href = "/passwordGen")}
+            variant="outlined"
+            sx={{ color: "black" }}
+          >
             Password generator
           </Button>
-          <Button variant="outlined" sx={{ color: "black" }}>
+          <Button
+            onClick={() => {
+              window.location.href = "/safetyChecker";
+            }}
+            variant="outlined"
+            sx={{ color: "black" }}
+          >
             Safety checker
           </Button>
         </Box>
