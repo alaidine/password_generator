109bcd26d61d249abaccefc3d875959b29a870ec
alaidine <aa.alimlinde@gmail.com>
2023-03-17 09:42:28 +0400

    you can navigate back to the home page from the password gen page

--- pages/passwordGen.tsx
+++ pages/passwordGen.tsx
@@ -1,1 +1,12 @@
-export default function passwordGenerator() {}
+import { Button, Typography } from "@mui/material";
+
+export default function passwordGenerator() {
+  return (
+    <>
+      <Button onClick={() => (window.location.href = "/")} variant="outlined">
+        Home
+      </Button>
+      <Typography>Password generator page</Typography>
+    </>
+  );
+}
