8a571488773e000deca5f39770fda2aac4eafcdd
alaidine <aa.alimlinde@gmail.com>
2023-03-18 23:14:29 +0400

    now you know how the password that the user want needs to look like, now you need to generate it

--- pages/passwordGen.tsx
+++ pages/passwordGen.tsx
@@ -1,12 +1,84 @@
-import { Button, Typography } from "@mui/material";
+import HomeButton from "@/components/HomeButton";
+import {
+  Box,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Typography,
+  Button,
+} from "@mui/material";
+import React, { useState } from "react";
+
+function CheckboxList() {
+  const [checkboxes, setCheckboxes] = useState([false, false, false]);
+
+  const handleCheckboxChange = (index: any) => (event: any) => {
+    const newCheckboxes = [...checkboxes];
+    newCheckboxes[index] = event.target.checked;
+    setCheckboxes(newCheckboxes);
+  };
+
+  return (
+    <>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checkboxes[0]}
+            onChange={handleCheckboxChange(0)}
+          />
+        }
+        label="use special characters"
+        labelPlacement="end"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checkboxes[1]}
+            onChange={handleCheckboxChange(1)}
+          />
+        }
+        label="use special characters"
+        labelPlacement="end"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={checkboxes[2]}
+            onChange={handleCheckboxChange(2)}
+          />
+        }
+        label="use special characters"
+        labelPlacement="end"
+      />
+      <Typography>
+        Checked checkboxes: {checkboxes.filter(Boolean).length}
+      </Typography>
+    </>
+  );
+}
 
 export default function passwordGenerator() {
   return (
     <>
-      <Button onClick={() => (window.location.href = "/")} variant="outlined">
-        Home
-      </Button>
-      <Typography>Password generator page</Typography>
+      <Container>
+        <HomeButton />
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="left"
+          minHeight="100vh"
+          sx={{
+            margin: "auto",
+            display: "flex",
+            gap: "6px",
+            flexDirection: "column",
+            width: "300px",
+          }}
+        >
+          <Typography>Password generator page</Typography>
+          <CheckboxList />
+        </Box>
+      </Container>
     </>
   );
 }
